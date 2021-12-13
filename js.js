79ee5192a4689575bd1bba3e29c22d0cb2518d38
Nico Pasqualini <pasqualinin9@gmail.com>
2021-12-13 17:38:48 +1100

    setting displayQuestion functions, timer and key press

--- js.js
+++ js.js
@@ -1,125 +1,103 @@
-var timer=document.getElementById(`timer`)
-var scoreElement=document.getElementById(`score`)
-var highestScore=document.getElementById(`highest-score`)
-var quizElement=document.getElementById(`quiz`)
-var submitElement=document.getElementById(`submit`)
-var questions=[
-    {
-      question: "Who invented JavaScript?",
-      answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
+var timerElement = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var myBtn=document.getElementById(`btn1`);
+var myBtn2=document.getElementById(`btn2`)
+var myBtn3=document.getElementById(`btn3`)
+var myBtn4=document.getElementById(`btn4`)
+var scoreEl=document.getElementById(`finalscore`)
+var questions= ["Who invented JavaScript?","Which one of these is a JavaScript package manager?","Which tool can you use to ensure code quality?"]
+var answers=[{a: "Douglas Crockford",b: "Sheryl Sandberg",c: "Brendan Eich"},
+              {a: "Node.js", b: "TypeScript", c: "npm"},
+              {a: "jQuery",b: "RequireJS",c: "ESLint"}]
+var solutions=["c","c","c"]
+var quizEl = document.createElement("div");
+var listEl = document.createElement("ol");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var question=document.createElement("p")
+var quizAnswer=document.createElement("p")
+quizAnswer.setAttribute("id","key")
+var score=0
+scoreEl.textContent=score
+// function to generate timer 
+function countdown() {
+  var timeLeft = 30;
+  var time = setInterval(function () {
+    if (timeLeft >= 1) {
+      timerElement.textContent = timeLeft;
+      timeLeft--;
+    } else {
+      timerElement.textContent = '';
+      clearInterval(time);
+      // Call the `displayMessage()` function
+      //nextQuestion2();
     }
-  ];
-
-//save last score
-localStorage.setItem("Score",score) 
-
-renderHighestScore()
-
-//retrieve highest score 
-function renderHighestScore(){
-    var score=localStorage.getItem("Score")
-if(actualScore>score){//need to declare actualScore
-  highestScore.textContent="Highest score ",actualScore
-}
-else{
-    highestScore.textContent="Highest score ",score
-}
+  }, 1000);
 }
 
-function buildQuiz(){
-    // variable to store the HTML output
-    var output = [];
-  
-    // for each question...
-    questions.forEach(
-      (currentQuestion, index) => {
-  
-        // variable to store the list of possible answers
-         answers = [];
-  
-        // and for each available answer...
-        for(letter in questions.answers){
-  
-          // ...add an HTML radio button
-          answers.push(
-            `<label>
-              <input type="radio" name="question${index}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
-          );
-        }
-  
-        // add this question and its answers to the output
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
-        );
-      }
-    );
-  
-    // finally combine our output list into one string of HTML and put it on the page
-    quizElement.innerHTML = output.join('');
+myBtn.addEventListener("click",countdown)
+
+//first question
+function nextQuestion1(){
+  countdown()
+  question.textContent=questions[0]
+  li1.textContent = answers[0].a;
+  li2.textContent = answers[0].b;
+  li3.textContent = answers[0].c;
+  document.body.appendChild(quizEl);
+  quizEl.appendChild(question);
+  quizEl.appendChild(listEl);
+  listEl.appendChild(li1);
+  listEl.appendChild(li2);
+  listEl.appendChild(li3);
+  document.body.appendChild(quizAnswer)
+  quizAnswer.textContent="Your answer: "
+}
+//second question 
+function nextQuestion2(){
+  question.textContent=questions[1]
+  li1.textContent = answers[1].a;
+  li2.textContent = answers[1].b;
+  li3.textContent = answers[1].c;
+  document.body.appendChild(quizEl);
+  quizEl.appendChild(question);
+  quizEl.appendChild(listEl);
+  listEl.appendChild(li1);
+  listEl.appendChild(li2);
+  listEl.appendChild(li3);
+  document.body.appendChild(quizAnswer)
+  quizAnswer.textContent="Your answer: "
   }
 
+  //third question 
+function nextQuestion3(){
+  question.textContent=questions[2]
+  li1.textContent = answers[2].a;
+  li2.textContent = answers[2].b;
+  li3.textContent = answers[2].c;
+  document.body.appendChild(quizEl);
+  quizEl.appendChild(question);
+  quizEl.appendChild(listEl);
+  listEl.appendChild(li1);
+  listEl.appendChild(li2);
+  listEl.appendChild(li3);
+  document.body.appendChild(quizAnswer)
+  quizAnswer.textContent="Your answer: "
+  }
 
+myBtn2.addEventListener("click",nextQuestion1)
+myBtn3.addEventListener("click",nextQuestion2)
+myBtn4.addEventListener("click",nextQuestion3)
 
-
-//countdown function to set the timer 
-function countdown(){
-    var timeleft=6
-    var timeInterval=setInterval(function(){
-    if(timeleft>=1){
-        timeleft--
-        timer.textContent=timeleft+" Seconds remaining"
-        
-    } 
-    else{
-        timer.textContent=""
-        clearInterval(timeInterval)
-        buildQuiz()}
-    },1000)
+document.addEventListener("keydown", keydownAction);
+//take the user input and validation
+function keydownAction(event) {
+  var keyPress = event.key;
+  if(keyPress==1||keyPress==2||keyPress==3){
+     document.querySelector("#key").textContent = "Your answer: "+keyPress;
+     score++
 }
-
-
-//displayQuestion after a set time interval 
-function displayQuestion(){
-    var i=0
-    var timeInterval=setInterval(function(){
-        if(question[i]===undefined){
-            clearInterval(timeInterval)
-        }
-        else{
-            quizElement.textContent=JSON.stringify(question[i][i])
-            i++
-        }
-
-    },2000)
+else {document.querySelector("#key").textContent = "Your answer is not valid"}
+  
 }
-
-
-
