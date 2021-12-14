ed89a678086c431361cae59898ee1dd54613c49e
Nico Pasqualini <pasqualinin9@gmail.com>
2021-12-14 17:23:58 +1100

    updating questions scroll and validating user choice

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var timerElement = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
-var myBtn=document.getElementById(`btn1`);
+
 var myBtn2=document.getElementById(`btn2`)
 var myBtn3=document.getElementById(`btn3`)
 var myBtn4=document.getElementById(`btn4`)
@@ -15,14 +15,16 @@ var listEl = document.createElement("ol");
 var li1 = document.createElement("li");
 var li2 = document.createElement("li");
 var li3 = document.createElement("li");
+
 var question=document.createElement("p")
 var quizAnswer=document.createElement("p")
 quizAnswer.setAttribute("id","key")
-var score=0
-scoreEl.textContent=score
+
+
 // function to generate timer 
+var timeLeft = 30;
 function countdown() {
-  var timeLeft = 30;
+  
   var time = setInterval(function () {
     if (timeLeft >= 1) {
       timerElement.textContent = timeLeft;
@@ -35,9 +37,6 @@ function countdown() {
     }
   }, 1000);
 }
-
-myBtn.addEventListener("click",countdown)
-
 //first question
 function nextQuestion1(){
   countdown()
@@ -49,10 +48,20 @@ function nextQuestion1(){
   quizEl.appendChild(question);
   quizEl.appendChild(listEl);
   listEl.appendChild(li1);
+  li1.addEventListener("click",function(){
+    timeLeft+=20
+  })
   listEl.appendChild(li2);
+  li2.addEventListener("click",function(){
+    timeLeft+=20
+  })
   listEl.appendChild(li3);
-  document.body.appendChild(quizAnswer)
-  quizAnswer.textContent="Your answer: "
+  li3.addEventListener("click",function(){
+    var message=document.createElement("p")
+    message.textContent="Correct"
+    li3.appendChild(message)
+    setTimeout(nextQuestion2, 3000);
+  })
 }
 //second question 
 function nextQuestion2(){
@@ -64,12 +73,21 @@ function nextQuestion2(){
   quizEl.appendChild(question);
   quizEl.appendChild(listEl);
   listEl.appendChild(li1);
+  li1.addEventListener("click",function(){
+    timeLeft+=20
+  })
   listEl.appendChild(li2);
+  li2.addEventListener("click",function(){
+    timeLeft+=20
+  })
   listEl.appendChild(li3);
-  document.body.appendChild(quizAnswer)
-  quizAnswer.textContent="Your answer: "
-  }
-
+  li3.addEventListener("click",function(){
+    var message=document.createElement("p")
+    message.textContent="Correct"
+    li3.appendChild(message)
+    setTimeout(nextQuestion3, 3000);
+  })
+}
   //third question 
 function nextQuestion3(){
   question.textContent=questions[2]
@@ -82,22 +100,38 @@ function nextQuestion3(){
   listEl.appendChild(li1);
   listEl.appendChild(li2);
   listEl.appendChild(li3);
-  document.body.appendChild(quizAnswer)
-  quizAnswer.textContent="Your answer: "
-  }
+  li1.addEventListener("click",function(){
+    timeLeft+=20
+  })
+  listEl.appendChild(li2);
+  li2.addEventListener("click",function(){
+    timeLeft+=20
+  })
+  listEl.appendChild(li3);
+  li3.addEventListener("click",function(){
+    var message=document.createElement("p")
+    message.textContent="Correct"
+    li3.appendChild(message)
+    setTimeout(function(){
+      alert("You finished!!")
+    }, 3000);
+  })
+}
+
 
 myBtn2.addEventListener("click",nextQuestion1)
-myBtn3.addEventListener("click",nextQuestion2)
-myBtn4.addEventListener("click",nextQuestion3)
+//myBtn3.addEventListener("click",nextQuestion2)
+//myBtn4.addEventListener("click",nextQuestion3)
 
-document.addEventListener("keydown", keydownAction);
+//document.addEventListener("keydown", keydownAction);
 //take the user input and validation
-function keydownAction(event) {
-  var keyPress = event.key;
-  if(keyPress==1||keyPress==2||keyPress==3){
-     document.querySelector("#key").textContent = "Your answer: "+keyPress;
-     score++
-}
-else {document.querySelector("#key").textContent = "Your answer is not valid"}
+//function keydownAction(event) {
+  //var keyPress = event.key;
+  //if(keyPress==1||keyPress==2||keyPress==3){
+    // document.querySelector("#key").textContent = "Your answer: "+keyPress;
+     //if(keyPress==3){
+     //score++}
+//}
+//else {document.querySelector("#key").textContent = "Your answer is not valid"}
   
-}
+//}
