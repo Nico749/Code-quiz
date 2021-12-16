509ad8b22081f744d343a276f0ce5d9818297990
Nico Pasqualini <pasqualinin9@gmail.com>
2021-12-16 17:57:20 +1100

    updating readme file and css changes

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Code-quiz
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Demo](#demo)
+* [Project Status](#project-status)
+* [Room for Improvement](#room-for-improvement)
+* [Contact Me](#contact-me)
+
+## General info
+
+The goal of the project is to create a program that allows users to play a quiz. By clicking the startign button, they will be displayed with a series of questions: if they answer right, the next question qill appear, otherwise extra 20 seconds will be added to the timer. Once they finish the quiz, they are able to save their username and score 
+
+## Technologies
+
+Project is created with:
+* HTML
+* JavaScript
+* CSS
+
+## Demo
+![Working demo](./jijo
+
+## Project Status
+* Complete
+
+## Room for Improvement
+Room for improvement:
+* Code refactoring necessary
+* Improve style 
+
+## Contact me 
+Created by [Nico Pasqualini](https://nico749.github.io/Personal-portfolio-/) - feel free to contact me!
+
--- js.js
+++ js.js
@@ -7,7 +7,7 @@ var questions= ["Who invented JavaScript?","Which one of these is a JavaScript p
 var answers=[{a: "Douglas Crockford",b: "Sheryl Sandberg",c: "Brendan Eich"},
               {a: "Node.js", b: "TypeScript", c: "npm"},
               {a: "jQuery",b: "RequireJS",c: "ESLint"}]
-var solutions=["c","c","c"]
+//var solutions=["c","c","c"]
 var quizEl = document.createElement("div");
 var listEl = document.createElement("ol");
 var li1 = document.createElement("li");
@@ -15,9 +15,6 @@ var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 
 var question=document.createElement("p")
-//var quizAnswer=document.createElement("p")
-//quizAnswer.setAttribute("id","key")
-
 
 // function to generate timer 
 var timeLeft = 30;
@@ -30,11 +27,10 @@ function countdown() {
     } else {
       timerElement.textContent = '';
       clearInterval(time);
-      // Call the `displayMessage()` function
-      //nextQuestion2();
       }
   }, 1000);
 }
+
 //first question
 function nextQuestion1(){
   countdown()
@@ -42,7 +38,13 @@ function nextQuestion1(){
   li1.textContent = answers[0].a;
   li2.textContent = answers[0].b;
   li3.textContent = answers[0].c;
+  question.setAttribute("style","font-size:30px;font-family:italic;font-weight:bold")
+  li1.setAttribute("style","font-size:30px;font-family:italic; ")
+  li2.setAttribute("style","font-size:30px;font-family:italic")
+  li3.setAttribute("style","font-size:30px;font-family:italic")
+  
   document.body.appendChild(quizEl);
+  quizEl.setAttribute("style","border:solid 5px red; border-radius:2%; box-shadow:10px 10px 10px 10px #b50505")
   quizEl.appendChild(question);
   quizEl.appendChild(listEl);
   listEl.appendChild(li1);
@@ -61,6 +63,7 @@ function nextQuestion1(){
     setTimeout(nextQuestion2, 1500);
   })
 }
+
 //second question 
 function nextQuestion2(){
   question.textContent=questions[1]
@@ -77,7 +80,8 @@ function nextQuestion2(){
     setTimeout(nextQuestion3, 1500);
   })
 }
-  //third question 
+
+//third question 
 function nextQuestion3(){
   question.textContent=questions[2]
   li1.textContent = answers[2].a;
@@ -93,12 +97,11 @@ function nextQuestion3(){
   listEl.appendChild(li3);
   li3.addEventListener("click",function(){
      setTimeout(takeScore, 1500);
+     
   })
 }
-
 myBtn2.addEventListener("click",nextQuestion1)
-myBtn1.addEventListener("click",takeScore)
-
+//myBtn1.addEventListener("click",takeScore)
 
 function takeScore() {
   var x = document.createElement("input")
@@ -112,6 +115,7 @@ function takeScore() {
   y.setAttribute("id","score")
   var text = document.createTextNode("Submit");
   btn.appendChild(text)
+  btn.setAttribute("style","margin-top:30px")
   document.body.appendChild(btn)
   document.body.appendChild(x)
   document.body.appendChild(y)
@@ -121,25 +125,23 @@ function takeScore() {
   var totalscore = document.querySelector("#score").value;
   
   btn.addEventListener("click", function(event) {
-  //alert("great")
   event.preventDefault();
   localStorage.setItem("username", nickname);
-  localStorage.setItem("totalscore", totalscore);
+  localStorage.setItem("totalscore", 33);
   renderLastScore();
   }
 );
 }
 
 function renderLastScore() {
-  //alert("ciao")
  var name = localStorage.getItem("username");
- // var score = localStorage.getItem("totalscore");
-  var x = document.createElement("p")
+ var score = localStorage.getItem("totalscore");
+ var x = document.createElement("p")
   var y= document.createElement("p")
   document.body.appendChild(x)
   document.body.appendChild(y)
   x.textContent = name;
-  //y.textContent = score;
+  y.textContent = score;
 }
 
 
